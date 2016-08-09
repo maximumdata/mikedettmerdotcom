2c593fa2c0a958f83bdecdaa9150a67bdc6c67b8
Mike Dettmer <mike@mikedettmer.com>
2016-08-09 16:06:15 -0400

    fixes package.json

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router(),
       testing = require('./testing/router.js');
 
-router.use('/testing', testing);
+router.use('/', testing);
 
 module.exports = router;
