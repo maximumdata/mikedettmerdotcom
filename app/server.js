b7032a6bd28ede1069af906ebbd3aae366bab599
Mike Dettmer <mike@mikedettmer.com>
2016-08-29 04:06:21 -0400

    adds cors

--- app/server.js
+++ app/server.js
@@ -32,6 +32,12 @@ app.use(passport.initialize());
 
 require('../config/passport')(passport);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', routes);
 
 app.use(function(req, res, next) {
