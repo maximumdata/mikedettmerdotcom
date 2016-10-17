96069e7549e8914430d603c07609dbf71b508f6e
Mike Dettmer <mike@mikedettmer.com>
2016-10-17 16:19:42 -0400

    adds route to verify facebook bot

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const blog = require('./api/v1/blog/router')
 const auth = require('./api/v1/auth/router')
+const bot = require('./bot/router')
 
 router.use('/api/v1/blog', blog)
 router.use('/api/v1/auth', auth)
+router.use('/bot', bot)
 
 module.exports = router
