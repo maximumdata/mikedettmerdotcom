8437ff736d919ffe7f6c6a4257d3d4b4f6407eb5
Mike Dettmer <mike@mikedettmer.com>
2018-03-12 21:25:07 -0400

    adds missing assets

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var app = express();
+const path = require('path');
+
+// Define the port to run on
+app.set('port', 2368);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
