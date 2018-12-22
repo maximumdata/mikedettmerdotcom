98e56bce6bd252a531f6a7aacab497d5afade26e
Mike Dettmer <mike.dettmer@gstv.com>
2018-12-22 13:42:37 -0500

    feat(#7) - closes #7, adds /api/v${version_number}/ in front of all routes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(router);
+app.use('/api/v1/', router);
 
 app.listen(port, () => {
 	console.log(`listening on: ${port}`);
