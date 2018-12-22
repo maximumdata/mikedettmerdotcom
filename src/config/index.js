841d7cd95270a352ad6cf6519675d22c1dff6f38
Mike Dettmer <mike.dettmer@gstv.com>
2018-12-22 14:26:05 -0500

    feat(#8) - adds babel build flow and env checking for dotenv

---
+++ src/config/index.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+let config = {
+	parsed: {}
+};
+
+if (process.env.NODE_ENV !== 'production') {
+	config = dotenv.config();
+
+	if (config.error) {
+		throw new Error(config.error);
+	}
+} else {
+	config.parsed = {
+		PORT: process.env.PORT,
+		DB_USER: process.env.DB_USER,
+		DB_PASS: process.env.DB_PASS,
+		DB_URL: process.env.DB_URL,
+		DB_NAME: process.env.DB_NAME,
+		API_VERSION: process.env.API_VERSION
+	};
+}
+
+export default config.parsed;
