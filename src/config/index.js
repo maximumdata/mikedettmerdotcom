7ad3e6ca1ade24ff88478dbc94618af3e8f23311
Mike Dettmer <mike.dettmer@gstv.com>
2018-12-23 00:32:18 -0500

    fix(#28) - adds missing protocol env var. closes #28

--- src/config/index.js
+++ src/config/index.js
@@ -11,7 +11,15 @@ if (process.env.NODE_ENV !== 'production') {
 		throw new Error(config.error);
 	}
 } else {
-	config.parsed = { PORT, DB_USER, DB_PASS, DB_URL, DB_NAME, DB_PROTOCOL, API_VERSION } = process.env;
+	config.parsed = {
+		PORT: process.env.PORT,
+		DB_USER: process.env.DB_USER,
+		DB_PASS: process.env.DB_PASS,
+		DB_URL: process.env.DB_URL,
+		DB_NAME: process.env.DB_NAME,
+		API_VERSION: process.env.API_VERSION,
+		PROTOCOL: process.env.PROTOCOL
+	};
 }
 
 export default config.parsed;
