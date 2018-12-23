49594dc2524e1102f95316e8852b811ac1d16a27
Mike Dettmer <mike.dettmer@gstv.com>
2018-12-23 00:04:33 -0500

    feat(#21) - adjusts scripts to use new prod env config, adds test for config, adjusts db connection strategy - will be cleaned in #24

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
 import config from '../config';
 
-mongoose.connect(`mongodb+srv://${config.DB_USER}:${config.DB_PASS}@${config.DB_URL}/${config.DB_NAME}`, { useNewUrlParser: true });
+if (process.env.NODE_ENV === 'development') {
+	mongoose.connect(`${config.DB_PROTOCOL}://${config.DB_USER}:${config.DB_PASS}@${config.DB_URL}/${config.DB_NAME}`, { useNewUrlParser: true });
+} else {
+	mongoose.connect(`${config.DB_PROTOCOL}://${config.DB_URL}/${config.DB_NAME}`, { useNewUrlParser: true });
+}
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
--- tests/config.spec.js
+++ tests/config.spec.js
@@ -4,4 +4,7 @@ describe('(Utils) ...config', () => {
 	it('should be an object', () => {
 		expect(config).to.be.a('object');
 	});
+	it('should return requested config item', () => {
+		expect(Number(config.PORT)).to.eql(2369);
+	});
 });
