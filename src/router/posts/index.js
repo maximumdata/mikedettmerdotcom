baf42cea9236c5c9732fc8b171bae54b164c9b0e
Mike Dettmer <mike.dettmer@gstv.com>
2018-12-29 00:02:14 -0500

    chore() - testing signing

--- src/router/posts/index.js
+++ src/router/posts/index.js
@@ -11,14 +11,16 @@ import {
 
 const router = Router();
 
+// probably not needed anymore
 router.get('/', getAllPublishedPosts);
-router.post('/', createNewPost);
 
-// specific post routes
-// router.use('/:id', addPostToReq);
-router.get('/:id', addPostToReq, getSinglePost);
+// needs auth
+router.post('/', createNewPost);
 router.delete('/:id', addPostToReq, deletePost);
 router.patch('/:id', addPostToReq, updatePost);
+
+// specific post routes
+router.get('/:id', addPostToReq, getSinglePost);
 
 // paginated routes
 router.use('/page/:page', getPostsByPage);
