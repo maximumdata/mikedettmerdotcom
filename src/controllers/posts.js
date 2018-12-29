30e7d334e11612c5b2f9080f4b64e6f1e70f5ddc
Mike Dettmer <mike@mikedettmer.com>
2018-12-29 00:14:13 -0500

    fix(#40) - hardens page param to always be either the supplied number or 1 if NaN

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -95,7 +95,8 @@ export async function addPostToReq(req, res, next) {
 
 export async function getPostsByPage(req, res) {
 	try {
-		const { page } = req.params;
+		let { page } = req.params;
+		page = Number(page) !== NaN ? Number(page) : 1;
 		const posts = await Posts.paginate({}, {
 			page,
 			limit: 5,
