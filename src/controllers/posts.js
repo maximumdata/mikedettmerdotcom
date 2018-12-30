8ca0bcc350930bec54afbeb1e29f325048972de2
Mike Dettmer <mike@mikedettmer.com>
2018-12-30 06:46:35 -0500

    fix(#42) - gonna have to walk away because im so fed up with codefactor right now

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -96,7 +96,7 @@ export async function addPostToReq(req, res, next) {
 export async function getPostsByPage(req, res) {
 	try {
 		let { page } = req.query;
-		if (!page || global.isNaN(Number(page))) {
+		if (!page || Number.isNaN(Number(page))) {
 			page = 1;
 		} else {
 			page = Number(page);
