fafa9e20c53426478878e2b07ae96938725a429d
codefactor-io <support@codefactor.io>
2019-01-22 00:40:38 +0000

    [CodeFactor] Apply fixes to commit a15334c

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 const database = new DB();
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
